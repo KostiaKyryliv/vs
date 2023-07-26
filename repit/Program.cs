fccd63cc39f01c7d78194e9ef9923eb68131e9c3
KostiaKyryliv <kyrylivkosia23@gmail.com>
2023-07-26 22:32:40 +0300

    right triangl

--- repit/Program.cs
+++ repit/Program.cs
@@ -21,7 +21,7 @@ namespace add1
                 
                 for (int i = 0; i < size; i++) 
                 { 
-                    for(int j = 0; j < size-i; j++)
+                    for(int j = 0; j <=i; j++)
                     {
                         Console.Write("*");
                     }
